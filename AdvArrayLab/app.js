7acf3669cdb76339ac103a2b80dbc98285e11e7c
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-28 14:01:25 -0600

    completed problem seven

--- AdvArrayLab/app.js
+++ AdvArrayLab/app.js
@@ -256,10 +256,22 @@ console.log('Cuisine Names: ', cuisineNames)
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
 //Map 
 
-function problemSeven
+function problemSeven(){
+
+    let results = dishes.map(function(el){
+        return el.cuisine + " " + el.name
+    })
+
+    return results
+}
+
+let cuisineAndName = problemSeven()
+console.log('Cuisine: ', cuisineAndName)
 
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
+
+
 
 
 //BONUS
